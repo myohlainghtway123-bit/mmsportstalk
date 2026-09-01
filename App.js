52d574574c0b686446f6f243a42032d354e5e827
Myo Hlaing Htwe <myohlainghtway123@gmail.com>
2026-09-02 01:52:39 +0700

    fix(scores): make current Phase 4B app the release entrypoint

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-const App = process.env.EXPO_PUBLIC_MST_INTERNAL === "true"
-  ? require("./src/phase4b/Phase4BScoresInternalAlpha").default
-  : require("./src/AppFinalShell").default;
+import Phase4BScoresInternalAlpha from "./src/phase4b/Phase4BScoresInternalAlpha";
 
-export default App;
+export default Phase4BScoresInternalAlpha;
